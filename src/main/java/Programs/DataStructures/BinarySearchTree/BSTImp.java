bf261d10a7b95e31f43272d1c12c071882ef8e46
Haritha Navaneetha Krishnan <haritha_navaneethakrishnan@trimble.com>
2024-01-16 12:13:52 +0530

    BST Inorder Parent, Inorder successor, max min , isBST

--- src/main/java/Programs/DataStructures/BinarySearchTree/BSTImp.java
+++ src/main/java/Programs/DataStructures/BinarySearchTree/BSTImp.java
@@ -7,9 +7,9 @@ class Node{
     Node right;
 }
 
-class BSTMethods{
+class BSTMethods {
 
-    public Node createNewNode(int value){
+    public Node createNewNode(int value) {
         Node node = new Node();
         node.data = value;
         node.left = null;
@@ -17,48 +17,42 @@ class BSTMethods{
         return node;
     }
 
-    public Node insertNode(Node node, int value){
-        if(node == null){
+    public Node insertNode(Node node, int value) {
+        if (node == null) {
             return createNewNode(value);
         }
-        if(value < node.data){
+        if (value < node.data) {
             node.left = insertNode(node.left, value);
-        }
-        else if(value > node.data){
+        } else if (value > node.data) {
             node.right = insertNode(node.right, value);
         }
 
         return node;
     }
 
-    public Node delete(Node node,int value){
-        if(node == null){
+    public Node delete(Node node, int value) {
+        if (node == null) {
             return null;
         }
 
         //if and else if for traversing
-        if(value < node.data){
+        if (value < node.data) {
             node.left = delete(node.left, value);
-        }
-        else if(value > node.data) {
+        } else if (value > node.data) {
             node.right = delete(node.right, value);
-        }
-        else{
+        } else {
 
             //If there is one or both nodes are empty then the below logic
-            if(node.left == null || node.right == null)
-            {
+            if (node.left == null || node.right == null) {
                 Node temp = null;
                 temp = node.left == null ? node.right : node.left;
 
-                if(temp == null ){
+                if (temp == null) {
                     return null;
-                }
-                else{
+                } else {
                     return temp;
                 }
-            }
-            else{
+            } else {
 
                 Node successor = getSuccessor(node);
                 node.data = successor.data;
@@ -69,62 +63,62 @@ class BSTMethods{
         return node;
     }
 
-    public Node getSuccessor(Node node){
+    public Node getSuccessor(Node node) {
 
-        if(node == null ){
+        if (node == null) {
             return null;
         }
         Node temp = node.right;
 
-        while(temp.left !=null){
+        while (temp.left != null) {
             temp = temp.left;
         }
         return temp;
     }
 
-    public void inorderTraversal(Node node){
-        if(node == null){
+    public void inorderTraversal(Node node) {
+        if (node == null) {
             return;
         }
 
         inorderTraversal(node.left);
-        System.out.print(node.data+ " ");
+        System.out.print(node.data + " ");
         inorderTraversal(node.right);
     }
 
-    public void preOrderTraversal(Node node){
-        if(node == null){
+    public void preOrderTraversal(Node node) {
+        if (node == null) {
             return;
         }
 
-        System.out.print(node.data+" ");
+        System.out.print(node.data + " ");
         preOrderTraversal(node.left);
         preOrderTraversal(node.right);
     }
 
-    public void postOrderTraversal(Node node){
-        if(node == null){
+    public void postOrderTraversal(Node node) {
+        if (node == null) {
             return;
         }
 
         postOrderTraversal(node.left);
         postOrderTraversal(node.right);
-        System.out.print(node.data+" ");
+        System.out.print(node.data + " ");
     }
 
-    public boolean isNodePresent(Node node, int value){
-        if(node == null){
+    public boolean isNodePresent(Node node, int value) {
+        if (node == null) {
             return false;
         }
 
         boolean isPresent = false;
 
-        while(node != null){
-            if(value < node.data){
+        while (node != null) {
+            if (value < node.data) {
                 node = node.left;
-            }else if(value > node.data){
+            } else if (value > node.data) {
                 node = node.right;
-            }else{
+            } else {
                 isPresent = true;
                 break;
             }
@@ -132,54 +126,148 @@ class BSTMethods{
         return isPresent;
     }
 
-    public Node getParentNode(Node node, int value){
-        if(node == null){
+    public Node getParentNode(Node node, int value) {
+        if (node == null) {
             return null;
         }
 
         Node getParent = null;
 
-        while(node != null){
-            if(value < node.data){
+        while (node != null) {
+            if (value < node.data) {
                 //Before shifting catch the parent Node
                 getParent = node;
                 node = node.left;
-            }
-            else if(value > node.data){
+            } else if (value > node.data) {
                 getParent = node;
                 node = node.right;
-            }
-            else{
+            } else {
                 break;
             }
         }
-        return node !=null ? getParent : null;
+        return node != null ? getParent : null;
     }
 
-    public Node getSiblingNode(Node node, int value){
-        if(node == null){
+    public Node getSiblingNode(Node node, int value) {
+        if (node == null) {
             return null;
         }
 
         Node parentNode = null;
 
-        while(node != null){
-            if(value < node.data){
+        while (node != null) {
+            if (value < node.data) {
                 parentNode = node;
                 node = node.left;
-            }
-            else if(value > node.data){
+            } else if (value > node.data) {
                 parentNode = node;
                 node = node.right;
-            }else{
+            } else {
                 break;
             }
         }
 
 
-        if(parentNode.left != null && value == parentNode.left.data){return parentNode.right;}
-        else if(parentNode.right  != null &&value == parentNode.right.data){ return parentNode.left;}
-        return  null;
+        if (parentNode.left != null && value == parentNode.left.data) {
+            return parentNode.right;
+        } else if (parentNode.right != null && value == parentNode.right.data) {
+            return parentNode.left;
+        }
+        return null;
+    }
+
+    public Node getInorderParentNode(Node node, int value) {
+        if (node == null) {
+            return null;
+        }
+
+        Node inorderParentNode = null;
+
+        while (node != null) {
+            if (value < node.data) {
+                inorderParentNode = node;
+                node = node.left;
+            } else if (value > node.data) {
+                node = node.right;
+            } else {
+                break;
+            }
+        }
+        return node != null ? inorderParentNode : null;
+    }
+
+    public Node getInorderSuccessorNode(Node node, int value){
+        if (node == null) {
+            return null;
+        }
+
+        Node inorderSuccessorNode = null;
+
+        while (node != null) {
+            if (value < node.data) {
+                inorderSuccessorNode = node;
+                node = node.left;
+            } else if (value > node.data) {
+                node = node.right;
+            } else {
+                if(node.right != null){
+                    inorderSuccessorNode = getSuccessor(node);
+                }
+                break;
+            }
+        }
+        return node != null ? inorderSuccessorNode : null;
+    }
+
+    Node prev = null;
+    public boolean isBSTree(Node node){
+        if(node == null){
+            return true;
+        }
+
+        if(!isBSTree(node.left)){
+            return false;
+        }
+
+        if(prev !=null && prev.data > node.data){
+            return false;
+        }
+
+        prev = node;
+
+        return isBSTree(node.right);
+    }
+
+    public int getDifference(Node node){
+        if(node == null){
+            return 0;
+        }
+
+        return node.data - getDifference(node.left) - getDifference(node.right);
+    }
+
+    public int getMax(Node node){
+        if(node == null){
+            System.out.println("Tree is empty");
+            return 0;
+        }
+
+        while(node.right != null){
+            node = node.right;
+        }
+
+        return node.data;
+    }
+
+    public int getMin(Node node){
+        if(node == null){
+            return 0;
+        }
+        while(node.left !=null){
+            node = node.left;
+        }
+
+        return node.data;
     }
 }
 
@@ -199,6 +287,9 @@ public class BSTImp {
         root = a.insertNode(root, 1);
         root = a.insertNode(root, 14);
         root = a.insertNode(root, 13);
+
+        System.out.println();
+        System.out.println("Is it a BST ?? "+a.isBSTree(root));
 
         //Deletion of element
         root = a.delete(root, 1);
@@ -242,11 +333,33 @@ public class BSTImp {
 
         //To find the inorder parent
         System.out.println();
-        Node inorderNode = a.getSiblingNode(root, 10);
+        Node inorderNode = a.getInorderParentNode(root, 3);
         if(inorderNode != null){
             System.out.println("Inorder parent node of the value is "+inorderNode.data);
         }else {
             System.out.println("Oops! inorder parent node not found");
         }
+
+
+        //To get the inorder successor
+        System.out.println();
+        Node inorderSuccessorNode = a.getInorderSuccessorNode(root, 6);
+        if(inorderSuccessorNode != null){
+            System.out.println("Inorder successor node of the value is "+inorderSuccessorNode.data);
+        }else {
+            System.out.println("Oops! inorder successor node not found");
+        }
+
+        System.out.println();
+        System.out.println("Get Difference even odd values "+a.getDifference(root));
+
+        //Get the max node
+        System.out.println();
+        System.out.println("Get the maximum node value "+a.getMax(root));
+
+        //Get the min node
+        System.out.println();
+        System.out.println("Get the minimum node value "+a.getMin(root));
+
     }
 }
